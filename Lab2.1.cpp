7528039835b79fbc098ef57aae7d944d980fbacb
Deniska <deniskaost2005@gmail.com>
2024-05-28 02:27:27 +0300

    Добавьте файлы проекта.

---
+++ Lab2.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    std::ifstream inputFile("C:/Users/Дениска/Desktop/a.txt");
+    if (!inputFile) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    double number;
+    int zeroCount = 0;
+    double negativeSum = 0.0;
+
+    while (inputFile >> number) {
+        if (number == 0.0) {
+            zeroCount++;
+        }
+        if (number < 0.0) {
+            negativeSum += number;
+        }
+    }
+
+    inputFile.close();
+
+    std::cout << "Количество нулевых элементов: " << zeroCount << std::endl;
+    std::cout << "Сумма отрицательных элементов: " << negativeSum << std::endl;
+
+    return 0;
+}
